8bea547d10dc2890db3dded7ea02056aca415558
strBCL1 <work.artem.popov@gmail.com>
2022-04-12 20:33:51 +0200

    3.35 - Dependency Injection using Spring Framework

--- sfg-di/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ sfg-di/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
 import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,6 +17,26 @@ public class SfgDiApplication {
 
 		String greeting = myController.sayHello();
 		System.out.println(greeting);
-	}
 
+		System.out.println("------- Property");
+
+		PropertyInjectedController propertyInjectedController = ctx.getBean("propertyInjectedController",
+				PropertyInjectedController.class);
+
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("------- Setter");
+
+		SetterInjectedController setterInjectedController = ctx.getBean("setterInjectedController",
+				SetterInjectedController.class);
+
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("------- Constructor");
+
+		ConstructorInjectedController constructorInjectedController = ctx.getBean("constructorInjectedController",
+				ConstructorInjectedController.class);
+
+		System.out.println(constructorInjectedController.getGreeting());
+	}
 }
--- sfg-di/src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ sfg-di/src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,10 +1,15 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
+//    @Autowired
+//    The @Autowired annotation is optional here, since in this class the constructor is the ONLY way to construct the bean
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
