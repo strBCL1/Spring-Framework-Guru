5d882d3032ff5b2b1aa163f84e0ffabdc071912a
strBCL1 <work.artem.popov@gmail.com>
2022-04-12 21:04:32 +0200

    3.37 - Primary Beans

--- sfg-di/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ sfg-di/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -16,8 +16,9 @@ public class SfgDiApplication {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 		MyController myController = (MyController) ctx.getBean("myController");
 
-		String greeting = myController.sayHello();
-		System.out.println(greeting);
+		System.out.println("------- PRIMARY bean");
+
+		System.out.println(myController.sayHello());
 
 		System.out.println("------- Property");
 
