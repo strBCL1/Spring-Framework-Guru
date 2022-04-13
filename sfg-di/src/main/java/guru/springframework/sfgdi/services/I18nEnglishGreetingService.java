3180ed8396f45cf72fe5349f0626ccfef121f903
strBCL1 <work.artem.popov@gmail.com>
2022-04-13 11:30:33 +0200

    5.75 - DI in Java Configuration

--- sfg-di/src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ sfg-di/src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,6 +1,15 @@
 package guru.springframework.sfgdi.services;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
+
 public class I18nEnglishGreetingService implements GreetingService {
+    private final EnglishGreetingRepository englishGreetingRepository;
+
+    public I18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository) {
+        this.englishGreetingRepository = englishGreetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
         return "Hello World in English!";
