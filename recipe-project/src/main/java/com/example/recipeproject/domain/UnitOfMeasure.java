77d851f09a672f7ee1d3122a57c1230594cf420a
strBCL1 <work.artem.popov@gmail.com>
2022-04-15 21:36:31 +0200

    8.140 Database Initialization with Spring

--- recipe-project/src/main/java/com/example/recipeproject/domain/UnitOfMeasure.java
+++ recipe-project/src/main/java/com/example/recipeproject/domain/UnitOfMeasure.java
@@ -1,8 +1,9 @@
 package com.example.recipeproject.domain;
 
-import org.springframework.beans.factory.annotation.Lookup;
-
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class UnitOfMeasure {
---
+++ recipe-project/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into category (description) values ('american'), ('italian'), ('mexican'), ('fast food');
+insert into unit_of_measure (uom) values ('teaspoon'), ('tablespoon'), ('cup'), ('pinch'), ('ounce');
